d666e89347c88a75ed83fb66cbc69f6d9c2e4754
essi <granath.essi@gmail.com>
2025-02-26 13:27:40 +0200

    jotain

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-print("hello world")
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    print("Hello")
+    return "<h1>Hello World!<h1>"
+
+@app.route('/sentiment', methods=['POST'])
+def get_sentiment():
+    input_data = request.json
+    print(input_data)
+
+    # here be sentiment analysis
+    # pickle
+
+    return {'input_data': input_data, 'message': 'hello!'}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
